929f5cc2ec17bca34a2166641f13c7cf624f50c2
Cory <Coryupham@users.noreply.github.com>
2017-03-19 21:43:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,115 +1,152 @@
+app.js
+DETAILS
+ACTIVITY
+app.js
+Sharing Info
+U
+
+General Info
+Type
+Javascript
+Size
+3 KB (3,582 bytes)
+Storage used
+0 bytesOwned by someone else
+Location
+Ro-BO
+Owner
+Unionjackjz
+Modified
+9:34 PM by Unionjackjz
+Opened
+4:34 PM by me
+Created
+4:03 PM
+Description
+Add a description
+Download permissions
+Viewers can download
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 const config = require("./config.json");
 
 function pluck(array) {
-    return array.map(function(item) { return item["name"]; });
+	return array.map(function(item) { return item["name"]; });
 }
+
 function hasRole(mem, role) {
-    if(pluck(mem.roles).includes(role)){
-        return true;
-    }else{
-        return false;
-    }
+  return (pluck(mem.roles).includes(role));
 }
 function commandIs(str, msg){
-    return msg.content.toLowerCase().startsWith("!" + str)
+	return msg.content.toLowerCase().startsWith("!" + str)
 }
 
 bot.on('ready', () => {
-  console.log('Bot Online!');
+	console.log('Bot Online!');
 });
 
 bot.on("guildMemberAdd", member =>{
-  let guild = member.guild;
-  message.guild.channels.find("name", "289247179921424384").sendMessage( `Welcome, ${member.user} to the server! `);
+	let guild = member.guild;
+	message.guild.channels.find("name", "289247179921424384").sendMessage( `Welcome, ${member.user} to the California VEX Teams Discord! `);
 });
 
-bot.on("guildCreate", guild =>{
-  console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user.username}`);
-});
-
-var meme = ['Tips more than 21S', '8686868686868686', 'lol worse than 8000H', 'spermflys', '62? more like 60X XD', 'GET GUD M8'];
+var meme = [
+  'Tips more than 21S',
+  '8686868686868686',
+  'lol worse than 8000H',
+  'spermflys',
+  '62? more like 60X XD',
+  'GIT GUD M8',
+  'not enough water',
+  'Coryz typing skill level'
+];
 
 bot.on('message', message => {
-  if(message.author.bot) return;
-  if(!message.content.startsWith(config.prefix)) return;
+	if(message.author.bot) return;
+	if(!message.content.startsWith(config.prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(config.prefix.length);
-  console.log(command);
+	let command = message.content.split(" ")[0];
+	command = command.slice(config.prefix.length);
+	console.log(command);
 
-  let args = message.content.split(" ").slice(1);
+	let args = message.content.split(" ").slice(1);
 
-  if(command === "add"){
-    let numArray = args.map(n=> parseInt(n));
-    let total = numArray.reduce( (p, c) => p+c);
-    message.channel.sendMessage(total);
+  if(command === "help") {
+    message.channel.sendMessage(
+    "**!meme** | Displays:ok_hand: :fire: meme\n**!ping** | pong\n**!say <message>** | I'll do whatever you say :wink:\n**!kick** | Bye bye :cry:\n**!purge <input>** | Clears messages from chat\n**!roll** | Roll the dice!");
   }
 
-  if(command === "say"){
-    let modRole = message.guild.roles.find("name", "Admin");
-    if(message.member.roles.has(modRole.id)){
-      message.channel.sendMessage(args.join(" "));
-    } else {
-      message.reply("You do not have access to this command.");
-    }
-
+  if(command === "heil") {
+    message.reply("you now accept Cory as your new Supreme Overlord. Bow Down!");
   }
 
-  if(command === "meh-meh"){
-    message.reply(meme[Math.floor((Math.random() * (meme.length)))]);
+	if(command === "say") {
+		if(hasRole(message.member, "Admin") || (message.member, "Mods")){
+      var placeHolder = args.join("");
+			message.channel.sendMessage(placeHolder);
+		}else {
+			message.reply("Don't tell me what to do :angry:");
+		}
+	}
 
+	if(command === "meme") {
+		message.reply(meme[Math.floor((Math.random() * meme.length))]);
+	}
+
+  if(command === "roll") {
+    var rolled = Math.floor((Math.random() * 100));
+    message.channel.sendMessage(':game_die: ' + rolled);
   }
 
-  if(command === "kick"){
-    let modRole = message.guild.roles.find("name", "Admin");
-    if(!hasRole(message.member, "Admin")){
-      return message.reply("You do not have access to this command.");
-    }
-    if(message.mentions.users.size === 0){
-      return message.reply("Please mention a user to kick");
-    }
-    let kickMember = message.guild.member(message.mentions.users.first());
-    if(!kickMember){
-      return message.reply("That user is not valid.");
-    }
-    if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")){
-      return message.reply("I do not have the permissions to do this.");
-    }
-    kickMember.kick().then(member =>{
-      message.reply(`${member.user.usernaem} was succesfully kicked.`);
-    }).catch(e =>{
-      console.error(e);
-    });
-  }
+	if(command === "kick") {
+		let modRole = message.guild.roles.find("name", "Admin");
+		if(!hasRole(message.member, "Admin")){
+			return message.reply("If you do that again I'll kick you :angry:");
+		}
+		if(message.mentions.users.size === 0){
+			return message.reply("Mention a user to kick");
+		}
+		let kickMember = message.guild.member(message.mentions.users.first());
+		if(!kickMember){
+			return message.reply("That isn't a user...");
+		}
+		if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")){
+			return message.reply("I do not have the permissions to do this.");
+		}
+		kickMember.kick().then(member =>{
+			message.reply(`${member.user.usernaem} was kicked`);
+			}).catch(e =>{
+			console.error(e);
+		});
+	}
 
-  if(command === "ping") {
-    message.channel.sendMessage('pong');
-  }
+	if(command === "ping") {
+		message.channel.sendMessage('pong');
+	}
 });
 
 
 bot.on('message', (message) => {
-    var args = message.content.split(/[ ]+/);
-    if(commandIs("purge", message)){
-            if(hasRole(message.member, "Admin") || (message.member, "Mods")){
-                if(args.length >= 3){
-                message.channel.sendMessage("You defined too many arguments. Usage:");
-                }else{
-                    var msg;
-                    if (args.length === 1){
-                        msg=2;
-                    }else{
-                        msg=parseInt(args[1]) + 1;
-                    }
-                    message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-                    }
-           }else {
-           message.channel.sendMessage("You must be an [Admin] to use this command.");
-        }
-    }
+	var args = message.content.split(/[ ]+/);
+	if(commandIs("purge", message)){
+		if(hasRole(message.member, "Admin") || (message.member, "Mods")){
+			if(args.length >= 3){
+				message.channel.sendMessage("You defined too many arguments. Usage:");
+				}else{
+				var msg;
+				if (args.length === 1){
+					msg=2;
+					}else{
+					msg=parseInt(args[1]) + 1;
+				}
+			message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+			}
+			}else {
+			message.channel.sendMessage("You must be an [Admin] to use this command.");
+		}
+	}
 });
 
 
